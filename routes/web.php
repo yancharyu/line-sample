47ce9a6b1b9c9a1204b9ee5526a3780356254a9f
yancharyu <s.ryu1135@gmail.com>
2021-04-10 17:10:16 +0900

    修正

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/line', [LineController::class, 'webhook']);
+Route::get('/line', [LineController::class, 'webhook']);
 Route::post('/line', [LineController::class, 'webhook']);
